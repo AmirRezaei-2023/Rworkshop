cdb349fb0084052f3d4e893577c7eef1fccef480
AmirRezaei97 <rezaei97amir76@gmail.com>
2023-09-16 14:28:22 +0330

    second version

--- minitutorial.R
+++ minitutorial.R
@@ -2,11 +2,11 @@
 getwd()
 
 # changing working directory 
-setwd('D:/quera')
+setwd('path')
 
 #Reading data into R by csv
-personaldata <- read.csv('person.csv')
-personaldata
+moviedata <- read.csv('movie.csv')
+moviedata
 
 # show the data frame
 View(personaldata)
@@ -20,5 +20,34 @@ names(personaldata)
 
 #removing the data frame 
 remove(personaldata)
+
+#merging data in R
+#Warning! this function is able to merge just two data frame at moment
+#read in another data frame
+genredata <- read.csv('genre.csv')
+genredata
+#Help =  you can use below command to get help in coding 
+#?merge
+mergeddf <- merge(x = genredata , y = moviedata , by.x='movie_id', by.y='id')
+mergeddf
+#Warning! if the common column has same name in both data frame you can use (by = 'column_name') easily
+new_mergeddf <- merge(x = moviedata , y = genredata , by.x='id', by.y='movie_id')
+new_mergeddf 
+#Warning! in inner join you dont need any option extra
+
+
+#full merge 
+full_mergeddf <- merge(x = moviedata , y = genredata , by.x='id', by.y='movie_id' , all=TRUE)
+full_mergeddf
+#Warning! by this,you get all rows even if there is not corresponding value in other data frame 
+
+#left merge 
+left_mergeddf <- merge(x = moviedata , y = genredata , by.x='id', by.y='movie_id' , all.x=TRUE)
+left_mergeddf
+
+#right merge 
+right_mergeddf <- merge(x = moviedata , y = genredata , by.x='id', by.y='movie_id' , all.y=TRUE)
+right_mergeddf
+
 
 
